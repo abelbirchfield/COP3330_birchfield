f4e9d9a9e0c06e81657d7be43c0142a1f9e74d3e
Abel Birchfield <abirchfield@knights.ucf.edu>
2020-09-08 17:15:43 -0400

    Initial commit on local COP3330_birchfield repository

---
+++ birchfield_p1/src/Decrypter.java
@@ -0,0 +1,31 @@
+public class Decrypter {
+
+    private String code;
+
+    public Decrypter(String code) {
+        this.code = code;
+    }
+
+    public String decrypt(String code) {
+        int digit1 = Character.getNumericValue(code.charAt(0));
+        int digit2 = Character.getNumericValue(code.charAt(1));
+        int digit3 = Character.getNumericValue(code.charAt(2));
+        int digit4 = Character.getNumericValue(code.charAt(3));
+
+        digit1 = reverseConvert(digit1);
+        digit2 = reverseConvert(digit2);
+        digit3 = reverseConvert(digit3);
+        digit4 = reverseConvert(digit4);
+//        System.out.println(digit1 + " " + digit2 + " " + digit3 + " " + digit4);
+
+        this.code = Integer.toString(digit3) + digit4 + digit1 + digit2;
+        return this.code;
+    }
+
+    public static int reverseConvert(int digit) {
+        digit += 3;
+        digit %= 10;
+        return digit;
+    }
+
+}
---
+++ birchfield_p1/src/Encrypter.java
@@ -0,0 +1,30 @@
+public class Encrypter {
+
+    private String code;
+
+    public Encrypter(String code) {
+        this.code = code;
+    }
+
+    public String encrypt(String code) {
+        int digit1 = Character.getNumericValue(code.charAt(0));
+        int digit2 = Character.getNumericValue(code.charAt(1));
+        int digit3 = Character.getNumericValue(code.charAt(2));
+        int digit4 = Character.getNumericValue(code.charAt(3));
+
+        digit1 = convert(digit1);
+        digit2 = convert(digit2);
+        digit3 = convert(digit3);
+        digit4 = convert(digit4);
+
+        this.code = Integer.toString(digit3) + digit4 + digit1 + digit2;
+        return this.code;
+    }
+
+    public static int convert(int digit) {
+        digit += 7;
+        digit %= 10;
+        return digit;
+    }
+
+}
