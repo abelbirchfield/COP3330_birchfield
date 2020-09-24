537bb8262ce22a34713ed170e263c2008aa5dd9b
Abel Birchfield <abirchfield@knights.ucf.edu>
2020-09-24 10:51:03 -0400

    Added Junit tests

--- birchfield_p2/src/BodyMassIndexTest.java
+++ birchfield_p2/src/BodyMassIndexTest.java
@@ -1,19 +1,60 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-//import static org.junit.jupiter.api.Assertions.*;
+class BodyMassIndexTest {
+    @Test
+    public void testBmiScoreUnderweight() {
+        BodyMassIndex b = new BodyMassIndex(70, 125) {
+            assertEquals(17.9, b.bmiScore());
+        }
+    }
 
-//class BodyMassIndexTest {
-//    @Test
-//    public void testBmiScore() {
-//        BodyMassIndex b = new BodyMassIndex(70, 175) {
-//            assertEquals(25.1, b.bmiScore());
-//        }
-//    }
-//
-//    @Test
-//    public void testBmiCategory() {
-//        BodyMassIndex b = new BodyMassIndex(70, 175) {
-//            assertEquals("Overweight", b.bmiCategory());
-//        }
-//    }
-//
-//}
+    @Test
+    public void testBmiScoreNormalWeight() {
+        BodyMassIndex b = new BodyMassIndex(70, 160) {
+            assertEquals(22.9, b.bmiScore());
+        }
+    }
+
+    @Test
+    public void testBmiScoreOverweight() {
+        BodyMassIndex b = new BodyMassIndex(70, 175) {
+            assertEquals(25.1, b.bmiScore());
+        }
+    }
+
+    @Test
+    public void testBmiScoreObesity() {
+        BodyMassIndex b = new BodyMassIndex(70, 210) {
+            assertEquals(30.1, b.bmiScore());
+        }
+    }
+
+    @Test
+    public void testBmiCategoryUnderweight() {
+        BodyMassIndex b = new BodyMassIndex(70, 125) {
+            assertEquals("Underweight", b.bmiCategory());
+        }
+    }
+
+    @Test
+    public void testBmiCategoryNormalWeight() {
+        BodyMassIndex b = new BodyMassIndex(70, 160) {
+            assertEquals("Normal weight", b.bmiCategory());
+        }
+    }
+
+    @Test
+    public void testBmiCategoryOverweight() {
+        BodyMassIndex b = new BodyMassIndex(70, 175) {
+            assertEquals("Overweight", b.bmiCategory());
+        }
+    }
+
+    @Test
+    public void testBmiCategoryObesity() {
+        BodyMassIndex b = new BodyMassIndex(70, 210) {
+            assertEquals("Obesity", b.bmiCategory());
+        }
+    }
+}
