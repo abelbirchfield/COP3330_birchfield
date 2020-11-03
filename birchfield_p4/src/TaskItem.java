15250246601fb5fbe2ef598fc763e77bfe486d5e
Abel Birchfield <abirchfield@knights.ucf.edu>
2020-11-03 12:55:00 -0500

    add methods in TaskList

--- birchfield_p4/src/TaskItem.java
+++ birchfield_p4/src/TaskItem.java
@@ -2,10 +2,16 @@ public class TaskItem {
     private String title;
     private String description;
     private String dueDate;
+    private boolean completed;
 
     public TaskItem(String title, String description, String dueDate) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
+        this.completed = false;
+    }
+
+    public void setCompleted(boolean complete) {
+        this.completed = complete;
     }
 }
--- birchfield_p4/src/TaskList.java
+++ birchfield_p4/src/TaskList.java
@@ -6,4 +6,28 @@ public class TaskList {
     public TaskList() {
         this.tasks = new ArrayList<TaskItem>();
     }
+
+    public ArrayList<TaskItem> getTasks() {
+        return tasks;
+    }
+
+    public void addTask(TaskItem newTask) {
+        tasks.add(newTask);
+    }
+    public void removeTask(int index) {
+        tasks.remove(index);
+    }
+
+    public void editTaskList(int index, String field, String newValue) {
+        //set variable field = newValue for object at index in tasks
+    }
+
+    public void complete(int index) {
+        tasks.get(index).setCompleted(true);
+    }
+    public void incomplete(int index) {
+        tasks.get(index).setCompleted(false);
+    }
+
+
 }
