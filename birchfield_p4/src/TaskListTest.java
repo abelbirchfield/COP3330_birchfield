e2d2c0680bd1d74911f4cef3d87fa42310bc206f
Abel Birchfield <abirchfield@knights.ucf.edu>
2020-11-06 11:24:20 -0500

    initial commit for TaskListTest

---
+++ birchfield_p4/src/TaskListTest.java
@@ -0,0 +1,138 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskListTest {
+
+    @Test
+    public void addingTaskItemsIncreasesSize() {
+        TaskList t = new TaskList();
+        //assertEquals();
+    }
+
+    @Test
+    public void completingTaskItemChangesStatus() {
+        TaskList t = new TaskList();
+        //assertEquals();
+    }
+
+    @Test
+    public void completingTaskItemFailsWithInvalidIndex() {
+        TaskList t = new TaskList();
+        //assertEquals();
+    }
+
+    @Test
+    public void editingTaskItemChangesValues() {
+        TaskList t = new TaskList();
+        //assertEquals();
+    }
+
+    @Test
+    public void editingTaskItemDescriptionChangesValue() {
+        TaskList t = new TaskList();
+        //assertEquals();
+    }
+
+    @Test
+    public void editingTaskItemDescriptionFailsWithInvalidIndex() {
+        TaskList t = new TaskList();
+        //assertEquals();
+    }
+
+    @Test
+    public void editingTaskItemDueDateChangesValue() {
+        TaskList t = new TaskList();
+        //assertEquals();
+    }
+
+    @Test
+    public void editingTaskItemDueDateFailsWithInvalidIndex() {
+        TaskList t = new TaskList();
+        //assertEquals();
+    }
+
+    @Test
+    public void editingTaskItemTitleChangesValue() {
+        TaskList t = new TaskList();
+        //assertEquals();
+    }
+
+    @Test
+    public void editingTaskItemTitleFailsWithInvalidIndex() {
+        TaskList t = new TaskList();
+        //assertEquals();
+    }
+
+    @Test
+    public void gettingTaskItemDescriptionFailsWithInvalidIndex() {
+        TaskList t = new TaskList();
+        //assertEquals();
+    }
+
+    @Test
+    public void gettingTaskItemDescriptionSucceedsWithValidIndex() {
+        TaskList t = new TaskList();
+        //assertEquals();
+    }
+
+    @Test
+    public void gettingTaskItemDueDateFailsWithInvalidIndex() {
+        TaskList t = new TaskList();
+        //assertEquals();
+    }
+
+    @Test
+    public void gettingTaskItemDueDateSucceedsWithValidIndex() {
+        TaskList t = new TaskList();
+        //assertEquals();
+    }
+
+    @Test
+    public void gettingTaskItemTitleFailsWithInvalidIndex() {
+        TaskList t = new TaskList();
+        //assertEquals();
+    }
+
+    @Test
+    public void gettingTaskItemTitleSucceedsWithValidIndex() {
+        TaskList t = new TaskList();
+        //assertEquals();
+    }
+
+    @Test
+    public void newTaskListIsEmpty() {
+        TaskList t = new TaskList();
+        //assertEquals();
+    }
+
+    @Test
+    public void removingTaskItemsDecreasesSize() {
+        TaskList t = new TaskList();
+        //assertEquals();
+    }
+
+    @Test
+    public void removingTaskItemsFailsWithInvalidIndex() {
+        TaskList t = new TaskList();
+        //assertEquals();
+    }
+
+    @Test
+    public void savedTaskListCanBeLoaded() {
+        TaskList t = new TaskList();
+        //assertEquals();
+    }
+
+    @Test
+    public void uncompletingTaskItemChangesStatus() {
+        TaskList t = new TaskList();
+        //assertEquals();
+    }
+
+    @Test
+    public void uncompletingTaskItemFailsWithInvalidIndex() {
+        TaskList t = new TaskList();
+        //assertEquals();
+    }
+
+}
