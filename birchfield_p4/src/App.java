d865bdea92916a5fdb105c8a3b9a72708b11cd2c
Abel Birchfield <abirchfield@knights.ucf.edu>
2020-11-09 19:34:06 -0500

    Implement reading and writing to files. Program now works correctly with valid input

--- birchfield_p4/src/App.java
+++ birchfield_p4/src/App.java
@@ -1,4 +1,8 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 
 public class App {
 
@@ -14,7 +18,8 @@ public class App {
             switch (option) {
                 case 1: option1();
                         break;
-                case 2:
+                case 2: option2();
+                        break;
                 case 3: done = true;
                         break;
                 default: System.out.println("That is not a valid choice.");
@@ -25,6 +30,43 @@ public class App {
     public static void option1() {
         TaskList tasks = new TaskList();
         System.out.println("new task list has been created");
+        interactWithTaskList(tasks);
+    }
+    public static void option2() {
+        input.nextLine();
+        TaskList tasks = new TaskList();
+        System.out.print("Enter the file name to load: ");
+        String fileName = input.nextLine();
+        try {
+            File file = new File(fileName);
+            Scanner reader = new Scanner(file);
+
+            int count = 0;
+            while (reader.hasNextLine()) {
+                String taskLine = reader.nextLine();
+                int start = taskLine.indexOf("[");
+                int stop1 = taskLine.indexOf("]");
+                int stop2 = taskLine.indexOf(":");
+                String dueDate = taskLine.substring(start+1, stop1);
+                String title = taskLine.substring(stop1+2, stop2);
+                String description = taskLine.substring(stop2+2);
+                TaskItem newTask = new TaskItem(title, description, dueDate);
+                tasks.addTask(newTask);
+                if(taskLine.contains("***")) {
+                    tasks.complete(count);
+                }
+                count++;
+            }
+            reader.close();
+            System.out.println("task list has been loaded");
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        interactWithTaskList(tasks);
+    }
+
+    public static void interactWithTaskList(TaskList tasks) {
         boolean keepGoing = true;
         while (keepGoing) {
             displayListOperationMenu();
@@ -32,7 +74,6 @@ public class App {
             keepGoing = doChoice(choice, tasks);
         }
     }
-
     public static void displayMainMenu() {
         System.out.print("\nMain Menu\n----------\n");
         System.out.println("1) create a new list");
@@ -67,7 +108,8 @@ public class App {
                     return true;
             case 6: choice6(tasks);
                     return true;
-            case 7: return true;
+            case 7: choice7(tasks);
+                    return true;
             case 8: return false;
             default: System.out.println("That is not a valid choice.");
                      return true;
@@ -75,7 +117,7 @@ public class App {
     }
 
     public static void choice1(TaskList tasks) {
-        System.out.println("Current Tasks\n----------\n\n");
+        System.out.println("Current Tasks\n----------\n");
         System.out.println(tasks);
     }
     public static void choice2(TaskList tasks) {
@@ -92,7 +134,7 @@ public class App {
     }
     public static void choice3(TaskList tasks) {
         input.nextLine();
-        System.out.println("Current Tasks\n----------\n\n");
+        System.out.println("Current Tasks\n----------\n");
         System.out.println(tasks);
         System.out.print("Which task will you edit? ");
         int indexToEdit = input.nextInt();
@@ -109,7 +151,7 @@ public class App {
     }
     public static void choice4(TaskList tasks) {
         input.nextLine();
-        System.out.println("Current Tasks\n----------\n\n");
+        System.out.println("Current Tasks\n----------\n");
         System.out.println(tasks);
         System.out.print("Which task will you remove? ");
         int indexToRemove = input.nextInt();
@@ -117,7 +159,7 @@ public class App {
     }
     public static void choice5(TaskList tasks) {
         input.nextLine();
-        System.out.println("Uncompleted Tasks\n----------\n\n");
+        System.out.println("Uncompleted Tasks\n----------\n");
         System.out.println(tasks.toStringUncompleted());
         System.out.print("Which task will you mark as completed? ");
         int indexToComplete = input.nextInt();
@@ -125,10 +167,28 @@ public class App {
     }
     public static void choice6(TaskList tasks) {
         input.nextLine();
-        System.out.println("Completed Tasks\n----------\n\n");
+        System.out.println("Completed Tasks\n----------\n");
         System.out.println(tasks.toStringCompleted());
         System.out.print("Which task will you unmark as completed? ");
         int indexToIncomplete = input.nextInt();
         tasks.incomplete(indexToIncomplete);
+    }
+    public static void choice7(TaskList tasks) {
+        input.nextLine();
+        System.out.print("Enter the filename to save as: ");
+        String fileName = input.nextLine();
+        try {
+            File file = new File(fileName);
+            if (!file.createNewFile()) {
+                System.out.println("File already exists");
+            }
+            FileWriter writer = new FileWriter(fileName);
+            writer.write(tasks.toString());
+            writer.close();
+            System.out.println("task list has been saved");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
