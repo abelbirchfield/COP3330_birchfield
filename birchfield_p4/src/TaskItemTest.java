5edc8b63a38a48de1691a8b05c07602d3ddc4aae
Abel Birchfield <abirchfield@knights.ucf.edu>
2020-11-10 21:32:56 -0500

    All test cases passed for TaskItemTest

--- birchfield_p4/src/TaskItemTest.java
+++ birchfield_p4/src/TaskItemTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -5,50 +6,58 @@ class TaskItemTest {
 
     @Test
     public void creatingTaskItemFailsWithInvalidDueDate() {
-        TaskItem t = new TaskItem("Test", "this is a description", "2020-11-09");
-        //assertEquals();
+        Assertions.assertThrows(InvalidDateException.class, () -> {
+            new TaskItem("Test", "this is a description", "2020-11-31");
+        });
     }
 
     @Test
     public void creatingTaskItemFailsWithInvalidTitle() {
-        TaskItem t = new TaskItem("Test", "this is a description", "2020-11-09");
-        //assertEquals();
+        Assertions.assertThrows(InvalidTitleException.class, () -> {
+            new TaskItem("", "this is a description", "2020-11-09");
+        });
     }
 
     @Test
     public void creatingTaskItemSucceedsWithValidDueDate() {
         TaskItem t = new TaskItem("Test", "this is a description", "2020-11-09");
-        //assertEquals();
+        assertEquals("2020-11-09", t.getDueDate());
     }
 
     @Test
     public void creatingTaskItemSucceedsWithValidTitle() {
         TaskItem t = new TaskItem("Test", "this is a description", "2020-11-09");
-        //assertEquals();
+        assertEquals("Test", t.getTitle());
     }
 
     @Test
     public void settingTaskItemDueDateFailsWithInvalidDate() {
         TaskItem t = new TaskItem("Test", "this is a description", "2020-11-09");
-        //assertEquals();
+        Assertions.assertThrows(InvalidDateException.class, () -> {
+            t.setDueDate("2020-11-31");
+        });
     }
 
     @Test
     public void settingTaskItemDueDateSucceedsWithValidDate() {
         TaskItem t = new TaskItem("Test", "this is a description", "2020-11-09");
-        //assertEquals();
+        t.setDueDate("2020-11-08");
+        assertEquals("2020-11-08", t.getDueDate());
     }
 
     @Test
     public void settingTaskItemTitleFailsWithInvalidTitle() {
         TaskItem t = new TaskItem("Test", "this is a description", "2020-11-09");
-        //assertEquals();
+        Assertions.assertThrows(InvalidTitleException.class, () -> {
+            t.setTitle("");
+        });
     }
 
     @Test
     public void settingTaskItemTitleSucceedsWithValidTitle() {
         TaskItem t = new TaskItem("Test", "this is a description", "2020-11-09");
-        //assertEquals();
+        t.setTitle("Title");
+        assertEquals("Title", t.getTitle());
     }
 
 }
